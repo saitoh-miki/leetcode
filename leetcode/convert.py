d8082c1eec22e7ed91d4015b660708698a8422d6
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2020-08-28 21:21:52 +0900

    fix:convert.py

--- leetcode/convert.py
+++ leetcode/convert.py
@@ -8,37 +8,55 @@ import fire
 
 
 def main():
-    """poetry run leetcode convert code__.py codes_"""
-    fire.Fire({"convert": convert, "make": make})
+    """poetry run leetcode convert code.py codes"""
+    fire.Fire({"convert": convert, "make_readme": make_readme, "make_code": make_code})
 
 
-def convert(file: str = "code.py", dir_="codes"):
+def convert(from_file: str = "code.py", to_dir="codes"):
     """code.pyを分解してファイルを作成
 
-    :param file: 対象ファイル, defaults to "code.py"
-    :param dir_: 出力先, defaults to "codes"
+    :param from_file: 対象ファイル, defaults to "code.py"
+    :param to_dir: 出力先, defaults to "codes"
     """
-    os.makedirs(dir_, exist_ok=True)
-    if not Path(file).exists():
-        print(f"Not found {file}")
+    os.makedirs(to_dir, exist_ok=True)
+    if not Path(from_file).exists():
+        print(f"Not found {from_file}")
         return
-    with open(file) as fp:
+    with open(from_file) as fp:
         lines = fp.read().splitlines()
     pre, num, title = 0, 0, ""
     for i, line in enumerate(lines):
         if not re.search(r"^#\s*%%", line):
             continue
         if num:
-            add_file(num, title, lines[pre:i], dir_)
+            add_file(num, title, lines[pre:i], to_dir)
         pre, num, title = i, 0, ""
         m = re.match(r"#\s*%*\s*\[(\d+)\.\s*([^]]+)\]\([^)]+\)\s*", line)
         if m:
             pre, num, title = i, int(m.group(1)), m.group(2)
     if num:
-        add_file(num, title, lines[pre:], dir_)
+        add_file(num, title, lines[pre:], to_dir)
+    make_readme(to_dir)
+
+
+def add_file(num: int, title: str, lines: List[str], to_dir: str):
+    title = re.sub(r"[^a-zA-Z0-9 ]", "", title).strip()
+    fnam = f"{to_dir}/{num:04}_{title.replace(' ', '_')}.py"
+    n = len(lines)
+    while n and not lines[n - 1]:
+        n -= 1
+    with open(fnam, "w", encoding="utf-8") as fp:
+        fp.write("\n".join(lines[: n + 1]))
+
+
+def make_readme(from_dir):
+    """ディレクトリ内のファイルからREADMEを作成
+
+    :param from_dir: 入力先
+    """
     shutil.copy(".README.md", "README.md")
     with open("README.md", "a", encoding="utf-8") as fp:
-        for fnam in sorted(Path(dir_).glob("*")):
+        for fnam in sorted(Path(from_dir).glob("*")):
             with fnam.open(encoding="utf-8") as gp:
                 line = gp.readline()
             if m := re.match(r"#\s*%*\s*\[(\d+)\.\s*([^]]+)\]\(([^)]+)\)\s*", line):
@@ -47,21 +65,12 @@ def convert(file: str = "code.py", dir_="codes"):
                 fp.write(f"- [{s}]({u}) - ([leetcode.com]({m.group(3)}))\n")
 
 
-def add_file(num: int, title: str, lines: List[str], dir_: str):
-    title = re.sub(r"[^a-zA-Z0-9 ]", "", title).strip()
-    fnam = f"{dir_}/{num:04}_{title.replace(' ', '_')}.py"
-    n = len(lines)
-    while n and not lines[n - 1]:
-        n -= 1
-    with open(fnam, "w", encoding="utf-8") as fp:
-        fp.write("\n".join(lines[: n + 1]))
-
-
-def make(file: str):
-    if not (pth := Path(file)).exists():
-        print(f"Not found {file}")
+def make_code(from_file: str, to_file: str = "code.py"):
+    """from_file→to_file"""
+    if not (pth := Path(from_file)).exists():
+        print(f"Not found {from_file}")
         return
-    with open("code_.py", "w") as fp:
+    with open(to_file, "w") as fp:
         for line in pth.read_text().splitlines():
             if m := re.match(r"\[\**(\d+)\.\s*([^]\*]+)\**\]\(([^)]+)\)\s*", line):
                 fp.write(f"# %% [{m.group(1)}. {m.group(2)}]({m.group(3)})\n")
